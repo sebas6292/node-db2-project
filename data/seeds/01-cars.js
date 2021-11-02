ac55f5fb65ed8ecbb844cf619230a16ac27b59b2
Sebastian <sebas6292@gmail.com>
2021-11-02 15:33:27 -0700

    truncate

--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,36 @@
 // STRETCH
+const cars = [
+    {
+        vins: '1111111111111',
+        make: 'toyota',
+        model: 'prius',
+        mileage: 215000,
+        title: 'clean',
+        transmission: 'manual',
+    },
+    {
+        vins: '1111111111111',
+        make: 'toyota',
+        model: 'corolla',
+        mileage: 115000,
+        title: 'salvage',
+    },
+    {
+        vins: '1111111111111',
+        make: 'ford',
+        model: 'focus',
+        mileage: 15000,
+    },
+]
+
+exports.seet = function(knex) {
+    return knex('cars')
+    .truncate().then(() => {
+        return knex('cars').insert(cars)
+    })
+}
+
+// exports.seet = async function(knex) {
+//     await knex('cars').truncate()
+//      await knex('cars').insert(cars)
+// }
