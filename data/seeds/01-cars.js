62e3962b2b5a83a328c51068a56d3867c730ebf1
Sebastian <sebas6292@gmail.com>
2021-11-02 17:06:01 -0700

    index is fleshed out

--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,7 +1,7 @@
 // STRETCH
 const cars = [
     {
-        vins: '1111111111111',
+        vin: '11111111111111111',
         make: 'toyota',
         model: 'prius',
         mileage: 215000,
@@ -9,28 +9,28 @@ const cars = [
         transmission: 'manual',
     },
     {
-        vins: '1111111111111',
+        vin: '11111111111111111',
         make: 'toyota',
         model: 'corolla',
         mileage: 115000,
         title: 'salvage',
     },
     {
-        vins: '1111111111111',
+        vin: '11111111111111111',
         make: 'ford',
         model: 'focus',
         mileage: 15000,
     },
 ]
 
-exports.seet = function(knex) {
-    return knex('cars')
-    .truncate().then(() => {
-        return knex('cars').insert(cars)
-    })
-}
-
-// exports.seet = async function(knex) {
-//     await knex('cars').truncate()
-//      await knex('cars').insert(cars)
+// exports.seed = function(knex) {
+//     return knex('cars')
+//     .truncate().then(() => {
+//         return knex('cars').insert(cars)
+//     })
 // }
+
+exports.seed = async function(knex) {
+    await knex('cars').truncate()
+     await knex('cars').insert(cars)
+}
